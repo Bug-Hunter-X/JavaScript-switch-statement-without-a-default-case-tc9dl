b7d96af73b6a4a26a8b09141132b78fad3f1bdf8
BugHunter-X <askzenai@gmail.com>
2024-12-10 10:56:09 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,40 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    throw new Error('Cannot divide by zero');
+  }
+  return a / b;
+}
+
+function calculate(a, b, operator) {
+  switch (operator) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+    default:
+      throw new Error('Invalid operator');
+  }
+}
+
+console.log(calculate(10, 2, '+')); // Output: 12
+console.log(calculate(10, 2, '-')); // Output: 8
+console.log(calculate(10, 2, '*')); // Output: 20
+console.log(calculate(10, 2, '/')); // Output: 5
+console.log(calculate(10, 0, '/')); // Throws an error: Cannot divide by zero
+console.log(calculate(10, 2, '%')); // Throws an error: Invalid operator
